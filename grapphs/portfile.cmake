78ca1f94570931e37c38aea8de7d370a228744a4
Ricardo Novak <rov@vizrt.com>
2024-03-17 15:34:29 +0000

    new: DUK-41: add grapphs port

---
+++ grapphs/portfile.cmake
@@ -0,0 +1,22 @@
+vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO Kadowns/grapphs
+        REF 0.1.5
+        SHA512 ff0cee1988f4b7c99fad47cabb06c9d85f274b2fe7ee909dc12b7724e1f7a681c6b9e26642d04d7c0804edca54f099554ab5e441b84ed1a59e1f35fdb18ad5ba
+        HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+        SOURCE_PATH ${SOURCE_PATH}
+        OPTIONS
+            -DGRAPPHS_COMPILE_TESTS=OFF
+            -DGRAPPHS_COMPILE_GRAPHVIZ=OFF
+            -DGRAPPHS_COMPILE_SVG=OFF
+            -DGRAPPHS_COMPILE_SAMPLES=OFF
+            -DGRAPPHS_VERSION=0.1.5
+)
+vcpkg_cmake_install()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(INSTALL "${SOURCE_PATH}/LICENSE.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/grapphs" RENAME copyright)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
