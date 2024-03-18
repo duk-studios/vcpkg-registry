84d18785c6597ec57a84a85540eb6c46d001a185
Ricardo Novak <rov@vizrt.com>
2024-03-18 14:18:08 +0000

    fix: DUK-41: fix graphs portfile versioning

--- ports/grapphs/portfile.cmake
+++ ports/grapphs/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO Kadowns/grapphs
-        REF 0.1.5
+        REF "${VERSION}"
         SHA512 ff0cee1988f4b7c99fad47cabb06c9d85f274b2fe7ee909dc12b7724e1f7a681c6b9e26642d04d7c0804edca54f099554ab5e441b84ed1a59e1f35fdb18ad5ba
         HEAD_REF master
 )
@@ -13,7 +13,7 @@ vcpkg_cmake_configure(
             -DGRAPPHS_COMPILE_GRAPHVIZ=OFF
             -DGRAPPHS_COMPILE_SVG=OFF
             -DGRAPPHS_COMPILE_SAMPLES=OFF
-            -DGRAPPHS_VERSION=0.1.5
+            -DGRAPPHS_VERSION=${VERSION}
 )
 vcpkg_cmake_install()
 
