50077ef396a20080965930e91795d95334a3375a
Ricardo Novak <ricardonovak@yahoo.com.br>
2025-09-24 19:57:33 +0200

    [duk] add 0.16.0

--- ports/duk/portfile.cmake
+++ ports/duk/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
         OUT_SOURCE_PATH SOURCE_PATH
         URL https://github.com/duk-studios/duk
-        REF 9eb8820833adabeb3bd22e388f7cdbcd089cb2cd
+        REF 80f9f304c07ec61eac51b6bb348520e605a01239
         HEAD_REF master
 )
 
@@ -9,7 +9,6 @@ vcpkg_cmake_configure(
         SOURCE_PATH ${SOURCE_PATH}
         OPTIONS
         -DDUK_BUILD_SAMPLE=OFF
-        -DDUK_FORMAT_FILES=OFF
 )
 vcpkg_cmake_install()
 
